4b51c78124b8af8538575ed869010e5c56248c81
lucas <lucas.m.maris@gmail.com>
2024-02-27 19:46:45 +0200

    Moving Docker, added photos

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,15 +5,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-
-  ],
-  build: {
-    minify: 'none',
-    terserOptions: {
-      compress: {
-        keep_fnames: /^.*/, // Use a regular expression to match all function names
-      },
-    },
-  },
+    react()
+  ]
 });
